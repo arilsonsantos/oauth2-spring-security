7be606ec55dcc9cc908ed1277f9f72334b514c7e
Arilson Santos <arilson@msn.com>
2020-07-08 09:06:35 -0300

    User registration and template

--- src/main/java/br/com/example/auth/security/oauth2poc/infra/resources/RegistrationResource.java
+++ src/main/java/br/com/example/auth/security/oauth2poc/infra/resources/RegistrationResource.java
@@ -4,11 +4,13 @@ import br.com.example.auth.security.oauth2poc.domain.User;
 import br.com.example.auth.security.oauth2poc.domain.dto.UserDto;
 import br.com.example.auth.security.oauth2poc.infra.resources.util.GenericResponse;
 import br.com.example.auth.security.oauth2poc.service.UserService;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@AllArgsConstructor
 @RequestMapping("/api")
 public class RegistrationResource {
 
