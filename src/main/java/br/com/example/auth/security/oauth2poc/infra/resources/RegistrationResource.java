bf8a97806199bf4c5430857bd17a5895a915f3d0
ARilson <arilson@msn.com>
2020-07-16 08:35:49 -0300

    Change user password

--- src/main/java/br/com/example/auth/security/oauth2poc/infra/resources/RegistrationResource.java
+++ src/main/java/br/com/example/auth/security/oauth2poc/infra/resources/RegistrationResource.java
@@ -1,5 +1,6 @@
 package br.com.example.auth.security.oauth2poc.infra.resources;
 
+import br.com.example.auth.security.oauth2poc.domain.TokenVerification;
 import br.com.example.auth.security.oauth2poc.domain.User;
 import br.com.example.auth.security.oauth2poc.domain.dto.UserDto;
 import br.com.example.auth.security.oauth2poc.infra.resources.util.GenericResponse;
@@ -34,8 +35,41 @@ public class RegistrationResource {
 
     @RequestMapping(method = RequestMethod.HEAD, path = "/resendConfirmationToken/{username}" )
     public ResponseEntity<Void> resendRegistrationToken(@PathVariable ("username") String username){
-        this.userService.generateNewVerificationToken(username);
+        this.userService.generateNewVerificationToken(username, 0);
         return ResponseEntity.noContent().build();
     }
+
+    //TODO change to reset user
+    @PostMapping("/resetPassword/users")
+    public ResponseEntity<Void> resetPassword (@RequestParam("username") String username){
+        this.userService.generateNewVerificationToken(username, 1);
+        return  ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/changePassword")
+    public ResponseEntity<GenericResponse> resetPassword (@RequestParam("id") String idUser, @RequestParam("token") String token){
+        final String result = userService.validatePasswordResetToken(idUser, token);
+        if (result == null){
+            return ResponseEntity.ok().body(GenericResponse.builder().message("success").build());
+        }
+        return  ResponseEntity.status(HttpStatus.SEE_OTHER).body(GenericResponse.builder().message(result.toString()).build());
+    }
+
+    @GetMapping("/savePassword")
+    public ResponseEntity<GenericResponse> savePassword (@RequestParam("token") String token, @RequestParam("password") String password){
+        final String result = userService.validateTokenVerification(token);
+        if (result != null){
+            return ResponseEntity.ok().body(GenericResponse.builder().message(result.toString()).build());
+        }
+        final TokenVerification tokenVerification = userService.getVerificationTokenByToken(token);
+        if (tokenVerification != null){
+            userService.changeUserPassword(tokenVerification.getUser(), password);
+        }
+
+        return ResponseEntity.noContent().build();
+    }
+
+
+
 
 }
